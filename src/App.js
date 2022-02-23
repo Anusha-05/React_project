6549a882b1af6cc66bd54d29992c9e3029799f7c
Anusha-05 <anusha051999@gmail.com>
2022-02-24 00:49:12 +0530

    insta-profile page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import './App.css';
 import React , { component } from 'react';
 //import Mycomponent from './components/Greet';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
-import Hello from './components/Hello';
+
+import Post from './components/post';
+
 
 function App() {
   return (
-    <div className="App">
-      {/* <Mycomponent /> */}
-     <Greet name='mom'/>
-     <Greet name='Dad'/>
-     <Greet name='Abi'/>
-     {/* <Welcome /> */}
-     {/* <Hello /> */}
-    </div>
+    <div className='app'>
+   <div className='app_header'>
+     <img  className="app_headerImage" width="150" height="100"    src="http://shimmeringsoul.weebly.com/uploads/1/3/0/3/13031044/1-xkmi4fb5vws6-my7b22lza_1.png"/>
+   </div>
+
+   <h1>Hello Damon</h1>
+
+   <Post />
+   </div>
   );
 }
 
---
+++ src/components/post.js
@@ -0,0 +1,13 @@
+import react from "react";
+import './post.css'
+function Post(){
+    return (
+      <div>
+          <h3>Username</h3>
+          <img  className="post_image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgBx7_OWIhDM6sYPGNdKpPGGDdSLH07PvmRw&usqp=CAU"/>
+          <h4 className="post_text"><strong>Username</strong>Loving this view!!</h4>
+      
+      </div>
+    )
+}
+export default Post
