00a0c111ba13af9bdaf98fbd012a04e2ddac534f
Anusha-05 <anusha051999@gmail.com>
2022-02-25 00:28:51 +0530

    story and post page added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React , { component } from 'react';
 //import Mycomponent from './components/Greet';
 
 import Post from './components/post';
-
+import Avatar from '@mui/material/Avatar';
 
 function App() {
   return (
@@ -12,9 +12,15 @@ function App() {
      <img  className="app_headerImage" width="150" height="100"    src="http://shimmeringsoul.weebly.com/uploads/1/3/0/3/13031044/1-xkmi4fb5vws6-my7b22lza_1.png"/>
    </div>
 
-   <h1>Hello Damon</h1>
+   <div className='post_stories'>
+   <Avatar className="post_logo" alt="" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8rSCbgUoRKCPNKlcRWE8lxnuaj_k1dtw8YH45RVQ9jwN-duV35NEwofKbJJELb3Z6jS4&usqp=CAU" />
+   <Avatar className="post_logo" alt="" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRc1FTOKvZTMPirwIsoy-APdeUsbvUMUT0RZq1oPyuo5FvhsFZxDct9ci3OEMPjD4uFdA&usqp=CAU" />
+   <Avatar className="post_logo" alt="" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiVxi0J5NVRHIUzXxU_sOsxqCReLdHQExKXMZKT7SnIUgBVW1Y08TFYDl_p4YMTGhyFTc&usqp=CAU" />
+   </div>
 
+   <Post  />
    <Post />
+   <Post/>
    </div>
   );
 }
--- src/components/post.js
+++ src/components/post.js
@@ -1,11 +1,16 @@
 import react from "react";
 import './post.css'
+import Avatar from '@mui/material/Avatar';
 function Post(){
     return (
-      <div>
-          <h3>Username</h3>
-          <img  className="post_image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgBx7_OWIhDM6sYPGNdKpPGGDdSLH07PvmRw&usqp=CAU"/>
-          <h4 className="post_text"><strong>Username</strong>Loving this view!!</h4>
+      <div className="post"> 
+        <div className="post_header">
+          <Avatar className="post_logo" alt="" src="http://www.goodmorningimagesdownload.com/wp-content/uploads/2020/11/Nature-Whatsapp-DP-Profile-Images-Download-10.jpg" />
+          <h3>username</h3>
+        </div>
+          
+          <img  className="post_image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6U5PJWfTR0yjxw4Mw884Qmq0U5gS7ifXgPpsb44FjsEDEOkSLGR9Dc-Hru1Lbvs1h67I&usqp=CAU" />
+          <h4 className="post_text"><strong>username</strong>Believe in yourself</h4>
       
       </div>
     )
