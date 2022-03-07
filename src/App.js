80ccbbab3615f520daf51b8bd22e5d682d4a3c69
Anusha-05 <anusha051999@gmail.com>
2022-03-08 00:25:10 +0530

    worked with props , arraay destructuring , useState and useeffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
-import React , { component } from 'react';
+import React , { component, useState } from 'react';
 //import Mycomponent from './components/Greet';
 
 import Post from './components/post';
 import Avatar from '@mui/material/Avatar';
 
 function App() {
+
+const [ posts , setPost ] = useState([
+  {
+    username:"damon", 
+     caption:"This is awesome!", 
+     imageUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6U5PJWfTR0yjxw4Mw884Qmq0U5gS7ifXgPpsb44FjsEDEOkSLGR9Dc-Hru1Lbvs1h67I&usqp=CAU"
+
+  },
+{
+  username:"Elena" ,
+ caption:"stay cool!!",
+ imageUrl:"https://captionbuddy.com/data/uploads/2020/11/Instagram-Positive-Quotes-05.png" 
+}
+])
+useEffect( ()=>{
+//run while page loads or changes in variable posts
+}, [])
+
+
   return (
     <div className='app'>
    <div className='app_header'>
@@ -18,9 +37,17 @@ function App() {
    <Avatar className="post_logo" alt="" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiVxi0J5NVRHIUzXxU_sOsxqCReLdHQExKXMZKT7SnIUgBVW1Y08TFYDl_p4YMTGhyFTc&usqp=CAU" />
    </div>
 
-   <Post  />
-   <Post />
-   <Post/>
+
+  
+
+{
+  posts.map(post => 
+    <Post username={post.username} caption={post.caption}  imageUrl={post.imageUrl}/>
+  )
+}
+   {/* <Post  username="damon"  caption="This is awesome!" imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6U5PJWfTR0yjxw4Mw884Qmq0U5gS7ifXgPpsb44FjsEDEOkSLGR9Dc-Hru1Lbvs1h67I&usqp=CAU" /> */}
+   {/* <Post  username="Elena" caption="stay cool!!" imageUrl="https://captionbuddy.com/data/uploads/2020/11/Instagram-Positive-Quotes-05.png" /> */}
+   {/* <Post/> */}
    </div>
   );
 }
